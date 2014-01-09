31643afb8d2183f8f5e1e6faca10a7c1455718ff
Thomas Noguer <thomas.noguer@gmail.com>
2014-01-09 18:54:32 +0100

    Heuristics specif

---
+++ Code/ant_colony/src/Heuristics.cpp
@@ -0,0 +1,30 @@
+/*
+ * Heuristics.cpp
+ *
+ *  Created on: 9 janv. 2014
+ *      Author: Thomas
+ */
+
+#include "Heuristics.h"
+
+Heuristics::Heuristics() {
+}
+
+Heuristics::~Heuristics() {
+}
+
+int* Heuristics::MinimumDueDateBatch(int* batchs, Job* jobs){
+	return batchs;
+}
+
+/*int* Heuristics::Jonhson(Job* jobs){
+
+}*/
+
+/*int* Heuristics::Batching(Job* jobs) {
+
+}*/
+
+int* Heuristics::NearestNeighbor(int* towns, int** K) {
+	return towns;
+}
---
+++ Code/ant_colony/src/Heuristics.h
@@ -0,0 +1,53 @@
+/*
+ * Heuristics.h
+ *
+ *  Created on: 9 janv. 2014
+ *      Author: Thomas Noguer
+ */
+
+#ifndef HEURISTICS_H_
+#include "Job.h"
+#define HEURISTICS_H_
+
+/** This class contains the tools and heuristics used to find the solution of our problem.
+ * @author Thomas Noguer
+ */
+class Heuristics {
+public:
+	Heuristics();
+	virtual ~Heuristics();
+
+	/**
+	 * The minimal due date batch scheduling algorithm.
+	 * @param batchs The sequence of batchs to be ordered.
+	 * @param jobs The jobs.
+	 * @return The ordered sequence of batchs following the minimal due date batch scheduling algorithm.
+	 */
+	int* MinimumDueDateBatch(int* batchs, Job* jobs);
+
+	/**
+	 * Johnson's algorithm.
+	 * @param jobs The list of jobs to be scheduled.
+	 * @return The scheduled sequence of id of jobs.
+	 */
+	int* Jonhson(Job* jobs);
+
+	/**
+	 * The batch heuristic.
+	 * @param The sequence of id of jobs already scheduled on the flowshop.
+	 * @param The list of jobs.
+	 * @return The batchs of id of jobs. (ex: batchs found: {1,2}{4}{3}, sequence returned: [2,1,2,1,4,1,3,0])
+	 */
+	int* Batching(Job* jobs);
+
+	/**
+	 * Nearest neighbor algorithm.
+	 * @param towns The list of towns to be scheduled.
+	 * @param K The distance matrix.
+	 * @return The sequence of id of towns ordered following the nearest neighbor algorithm.
+	 */
+	int* NearestNeighbor(int* towns, int** K);
+
+};
+
+#endif /* HEURISTICS_H_ */
